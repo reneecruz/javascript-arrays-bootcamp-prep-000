b08075cc9ca8b27e2a044eb10d3b31fd806c9280
Renée Cruz <reneebasscruz@gmail.com>
2019-02-18 02:53:16 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -37,7 +37,7 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromEndOfArray (array) {
- return array.slice(1, array.length - 1);
+ return array.slice(0, array.length - 1);
  }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
